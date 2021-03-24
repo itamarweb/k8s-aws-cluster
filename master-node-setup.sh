43903dc2c13ddd572befb44e97c46048e8c1ae38
itamarweb <46376023+itamarweb@users.noreply.github.com>
2021-03-24 07:27:24 +0200

    first commit

---
+++ master-node-setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+
+# ------------------------------------ Docker installation --------------------------------------------
+sudo apt-get update
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "Docker installation finished"
+
+# ------------------------------------ END: Docker installation --------------------------------------------
+
+
+
+# ------------------------------------     K8S installation     --------------------------------------------
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+# ------------------------------------  END: K8S installation   --------------------------------------------
+
+
+#Networking setup
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+#Initialize the cluster
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
